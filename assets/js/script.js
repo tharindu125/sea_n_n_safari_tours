fb16a85a8f40e9c89b2bc8cced0c88cb55cf1aeb
Tharindu Ranaweera <nawanjanaenda@gmail.com>
2026-06-21 11:48:44 +0530

    add google business

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,14 +14,14 @@ const SEO_CONFIG = {
   ga4Id: 'G-BHSB4E2ZJV',
   googleSiteVerification: '', // meta content from Search Console
   bingSiteVerification: '', // meta content from Bing Webmaster Tools (msvalidate.01)
-  googleBusinessProfileUrl: '', // e.g. https://maps.app.goo.gl/... or Google Maps place URL
-  googleBusinessReviewUrl: '' // e.g. https://search.google.com/local/writereview?placeid=ChIJ...
+  googleBusinessProfileUrl: 'https://share.google/qEmeISXIeBbjaFtfo',
+  googleBusinessReviewUrl: 'https://g.page/r/CW28g6U2i504EAE/review'
 };
 const GOOGLE_GA_ID = SEO_CONFIG.ga4Id;
 const TWITTER_HANDLE = '';
 const SITE_COPYRIGHT = `\u00A9 ${new Date().getFullYear()} Sea & Safari Tours`;
 const SITE_ALTERNATE_NAME = 'Sea Safari Tours Mirissa';
-const GOOGLE_REVIEW_URL = 'https://www.google.com/search?q=Sea+%26+Safari+Tours+Mirissa+reviews';
+const GOOGLE_REVIEW_URL = SEO_CONFIG.googleBusinessReviewUrl;
 const TRIPADVISOR_URL = 'https://www.tripadvisor.com/Search?q=Sea+Safari+Tours+Mirissa';
 const FACEBOOK_URL = 'https://www.facebook.com/share/1BRVpgiRco/';
 const FACEBOOK_ICON_SVG = '<svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>';
